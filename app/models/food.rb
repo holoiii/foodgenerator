7014f44a9f17c0adf6b43fc2d5310760ea06971d
Wilson Chau <wilsonchau2@gmail.com>
2012-02-20 03:26:39 -0800

    new generate logic for food

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,22 +2,25 @@ class Food < ActiveRecord::Base
   CALORIES_PER_PERSON = 500
 
   def self.generate(budget, people, opts={})
-    total_calories = people * CALORIES_PER_PERSON
-    food_array = []
+    temp_calories = 0
     temp_budget = 0
-    10.times do
-      temp_calories = 0
-      temp_budget = 0
-      food_array = []
-      until temp_calories >= total_calories
-        rand_id = rand(Food.count)
-        food_item = Food.first(:conditions => ["id >= ?", rand_id])
+    food_array = []
+    used_food_array = []
+    while (temp_budget < budget) && (used_food_array.count < Food.count)
+      rand_id = rand(Food.count+1)
+      food_item = Food.first(:conditions => ["id >= ?", rand_id])
+      if used_food_array.include?(food_item) || food_array.include?(food_item)
+        #tried the food item already
+      elsif ((temp_budget + food_item.cost) > budget)
+        used_food_array << food_item
+        #skip to next food
+      else
+        used_food_array << food_item
         food_array << food_item
-        temp_calories += food_item.calories
         temp_budget += food_item.cost
       end
-      break if temp_budget <= budget
     end
-    temp_budget <= budget ? food_array : nil
+    food_array
   end
 end
+

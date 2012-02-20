68881a63ce7b12f802a80095fc7e708c00dadf60
Wilson Chau <wilsonchau2@gmail.com>
2012-02-20 02:15:35 -0800

    fixed migration

--- db/migrate/20120220082839_create_foods.rb
+++ db/migrate/20120220082839_create_foods.rb
@@ -3,6 +3,7 @@ class CreateFoods < ActiveRecord::Migration
     create_table :foods do |t|
       t.string :name
       t.integer :cost
+      t.integer :servings
       t.integer :calories
       t.boolean :vegetarian
       t.timestamps

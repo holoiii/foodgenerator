857a115fd58e2291e2387b9ab2e842cfc2b92624
Albert Peng <albert.peng@halogennetwork.com>
2012-02-20 03:00:43 -0800

    search uses ajax

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -1,12 +1,10 @@
 class GeneratorController < ApplicationController
   def search
-    @budget = params[:budget].to_i
-    @people = params[:people].to_i
-    @results = Food.generate(@budget, @people)
+    @results = Food.generate(params[:budget].to_i, params[:people].to_i)
     @total_cost = "$" + @results.map(&:cost).inject(&:+).to_s
     @total_servings = @results.map(&:servings).inject(&:+).to_s
     @total_calories = @results.map(&:calories).inject(&:+).to_s
-    @people_range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    render "home/index"
+
+    render :partial => "home/results"
   end
 end

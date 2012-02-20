3174a486697dcd8170bf8b91cf8a857b5f22b18d
Albert Peng <albert.peng@halogennetwork.com>
2012-02-20 02:32:21 -0800

    more styling, persist search info

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -1,8 +1,11 @@
 class GeneratorController < ApplicationController
   def search
-    budget = params[:budget].to_i
-    people = params[:people].to_i
-    @results = Food.generate(budget, people)
+    @budget = params[:budget].to_i
+    @people = params[:people].to_i
+    @results = Food.generate(@budget, @people)
+    @total_cost = "$" + @results.map(&:cost).inject(&:+).to_s
+    @total_servings = @results.map(&:servings).inject(&:+).to_s
+    @total_calories = @results.map(&:calories).inject(&:+).to_s
     @people_range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     render "home/index"
   end

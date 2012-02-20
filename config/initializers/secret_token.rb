af30b43d5dcdf6cc9e4116e301d806c5d7ce8521
Albert Peng <albert.peng@halogennetwork.com>
2012-02-19 23:50:28 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodgenerator::Application.config.secret_token = '3851b1909c6b1043149517c8337d1c2cfb23c8257467eb93914145f443682657a876ae242ac37148f577ed60a554ac4677a058e3ac63fd528fb7c4f3ee740bdf'
